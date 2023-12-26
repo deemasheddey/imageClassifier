ec821173ca390bbfcb3a1a261373cb23ae61a495
deemasheddey <91229548+deemasheddey@users.noreply.github.com>
2023-12-27 00:19:29 +0300

    Update data_mang.py

--- ImageClassifier/data_mang.py
+++ ImageClassifier/data_mang.py
@@ -1,57 +1,65 @@
 import torch
-from torchvision import datasets, transforms, models
+from torchvision import ds, trans, models
 from PIL import Image
 
 def loading_data(path):
-    print("Loading and preprocessing data from {} ...".format(path))
+    print("Now the data is loading {} ...".format(path))
     
     train_dir = path + '/train'
     valid_dir = path + '/valid'
     test_dir = path + '/test'
     
-    # Define transforms for the training, validation, and testing sets
-    train_transform = transforms.Compose([transforms.RandomRotation(50),
-                                                  transforms.RandomResizedCrop(224),
-                                                  transforms.RandomHorizontalFlip(),
-                                                  transforms.ToTensor(),
-                                                  transforms.Normalize([0.485, 0.456, 0.406],
-                                                                       [0.229, 0.224, 0.225])])
+    tr_transform = trans.Compose([trans.RandomRotation(50),trans.RandomResizedCrop(224), trans.RandomHorizontalFlip(), trans.ToTensor(), trans.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
-    valid_transform = transforms.Compose([transforms.Resize(255),
-                                                    transforms.CenterCrop(224),
-                                                    transforms.ToTensor(),
-                                                    transforms.Normalize([0.485, 0.456, 0.406],
-                                                                         [0.229, 0.224, 0.225])])
 
-    test_transform = transforms.Compose([transforms.Resize(255),
-                                                 transforms.CenterCrop(224),
-                                                 transforms.ToTensor(),
-                                                 transforms.Normalize([0.485, 0.456, 0.406],
-                                                                      [0.229, 0.224, 0.225])])
 
-    # Load the datasets with ImageFolder
-    train_data = datasets.ImageFolder(train_dir, transform = train_transform)
-    valid_data = datasets.ImageFolder(valid_dir, transform = valid_transform)
-    test_data = datasets.ImageFolder(test_dir, transform = test_transform)
+    
+##### NOW valid trans
+    val_transform = trans.Compose([trans.Resize(255),trans.CenterCrop(224), trans.ToTensor(),trans.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
-    # Using the image datasets and the trainforms, define the dataloaders
-    trainloader = torch.utils.data.DataLoader(train_data, batch_size = 64, shuffle = True)
-    validloader = torch.utils.data.DataLoader(valid_data, batch_size = 64)
-    testloader = torch.utils.data.DataLoader(test_data, batch_size = 64)
+
+
+
+
     
-    print("Finished loading and preprocessing data.")
+##### NOW test trans
+    test_transform = trans.Compose([trans.Resize(255),trans.CenterCrop(224), trans.ToTensor(), trans.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])])
+
+
+''''define each data with loader loader'''
+
     
-    return train_data, trainloader, validloader, testloader
-def processing_image(image):
-    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
-        returns an Numpy array
-    '''
-    image = Image.open(image)
+    trdata = ds.ImageFolder(train_dir, transform = tr_transform)
+    trloader = torch.utils.data.DataLoader(trdata, batch_size = 65, shuffle = True)
+
+    vldata = ds.ImageFolder(valid_dir, transform = val_transform)
+    valloader = torch.utils.data.DataLoader(vldata, batch_size = 65)
+
+
+
+    tstData = ds.ImageFolder(test_dir, transform = test_transform)
+    tstloader = torch.utils.data.DataLoader(tstData, batch_size = 65)
     
-    image_transform = transforms.Compose([transforms.Resize(255),
-                                                 transforms.CenterCrop(224),
-                                                 transforms.ToTensor(),
-                                                 transforms.Normalize([0.485, 0.456, 0.406],
-                                                                      [0.229, 0.224, 0.225])])
     
-    return image_transform(image)
+
+
+    
+    print("****Finished loading and preprocessing data****")
+    
+    return trdata, trloader, valloader, tstloader
+
+
+
+
+##############
+'''process image'''
+
+
+def processing_image(img):
+    img = Image.open(img)
+    image_transform = trans.Compose([trans.Resize(255), trans.CenterCrop(224), trans.ToTensor(),  trans.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])])
+    return image_transform(img)
+
+
+
+''''' THIS CLASS IS DONE'''' 
